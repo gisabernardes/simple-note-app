084acf2c6218a9e5eb24679e68d4f06ce08d0afb
Gisele Bernardes <giiselebernardes@gmail.com>
2019-07-03 14:35:25 -0300

    final app

--- app.js
+++ app.js
@@ -11,17 +11,23 @@ $(document).ready(function(){
   $('#btn-save').click(function(){
     var body = $('#body-field').val();
     if (body === '') {
-      alert ('Please add a title or body to your note.');
+      alert ('Please write a note.');
       return;
     }
     var color = $('#body-field').css('background-color');
     var id = noteCount + 1;
     if (activeNote) {
-      $('#' + activeNote)[0].children[0].innerHTML = body;
-      $('#' + activeNote)[0].style.backgroundColor = color;
+      $('#' + activeNote)[0].children[0].children[0].innerHTML = body;
+      $('#' + activeNote)[0].children[0].style.backgroundColor = color;
+      document.getElementById("btn-icon").classList.remove("fa-remove");
+      document.getElementById("btn-icon").classList.add("fa-eraser");
       activeNote = null;
     } else {
-      $('#listed').append('<div id="note' + id + '" style="background-color: ' + color + '"><div class="list-text">' + body + '</div></div>');
+      $('#listed').append('<div id="note' + id + '" class="card shadow-sm rounded">'
+                        +   '<div style="background-color: ' + color + '">'
+                        +     '<div class="card-text p-3">' + body + '</div>'
+                        +   '</div>'
+                        + '</div>');
       noteCount++;
     };
     $('#body-field').val('');
@@ -31,6 +37,8 @@ $(document).ready(function(){
   $('#btn-delete').click(function(){
     if (activeNote) {
       $('#' + activeNote)[0].remove();
+      document.getElementById("btn-icon").classList.remove("fa-remove");
+      document.getElementById("btn-icon").classList.add("fa-eraser");
       activeNote = null;
     }
       $('#body-field').val('');
@@ -38,12 +46,14 @@ $(document).ready(function(){
   });
 
   $('#listed').click(function(e){
-    var id = e.target.parentElement.id;
+    var id = e.target.parentElement.parentElement.id;
     var color = e.target.parentElement.style.backgroundColor;
     activeNote = id;
-    var bodySel = $('#' + id)[0].children[0].innerHTML;
+    var bodySel = $('#' + id)[0].children[0].children[0].innerHTML;
     $('#body-field').val(bodySel);
     $('#body-field').css('background-color', color);
-  })
+    document.getElementById("btn-icon").classList.remove("fa-eraser");
+    document.getElementById("btn-icon").classList.add("fa-remove");
+  });
 
 })
