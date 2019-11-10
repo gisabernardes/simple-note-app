ab99873f0c521e3c6a88a19d257cccb1c7b4d376
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2019-11-10 11:37:36 -0300

    updates

--- app.js
+++ app.js
@@ -1,59 +1,65 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   var noteCount = 0;
   var activeNote = null;
-
-  $('.color-box').click(function(){
-    var color = $(this).css('background-color');
-    $('#body-field').css('background-color', color);
-  })
-
-  $('#btn-save').click(function(){
-    var body = $('#body-field').val();
+  var clickButton = (function () {
+    var body = $('#input-field').val();
     if (body === '') {
-      alert ('Please write a note.');
+      alert('Please write a note.');
       return;
     }
-    var color = $('#body-field').css('background-color');
+    var color = $('#input-field').css('background-color');
     var id = noteCount + 1;
     if (activeNote) {
-      $('#' + activeNote)[0].children[0].children[0].innerHTML = body;
+      $('#' + activeNote)[0].children[0].textContent = body;
       $('#' + activeNote)[0].children[0].style.backgroundColor = color;
       document.getElementById("btn-icon").classList.remove("fa-remove");
       document.getElementById("btn-icon").classList.add("fa-eraser");
       activeNote = null;
     } else {
-      $('#listed').append('<div id="note' + id + '" class="card shadow-sm rounded">'
-                        +   '<div style="background-color: ' + color + '">'
-                        +     '<div class="card-text p-3">' + body + '</div>'
-                        +   '</div>'
-                        + '</div>');
+      $('#listed').append('<div id="note' + id + '" class="card shadow-sm rounded" style="background-color: ' + color + '">'
+        + '<div class="card-text p-3">' + body + '</div>'
+        + '</div>');
       noteCount++;
     };
-    $('#body-field').val('');
-    $('#body-field').css('background-color', 'white');
+    $('#input-field').val('');
+    $('#input-field').css('background-color', 'white');
   });
 
-  $('#btn-delete').click(function(){
+  $('.color-box').click(function () {
+    var color = $(this).css('background-color');
+    $('#input-field').css('background-color', color);
+  });
+
+  $('#input-field').keypress(function (event) {
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == '13') {
+      clickButton();
+    }
+  });
+
+  $('#btn-save').click(clickButton);
+
+  $('#btn-delete').click(function () {
     if (activeNote) {
       $('#' + activeNote)[0].remove();
       document.getElementById("btn-icon").classList.remove("fa-remove");
       document.getElementById("btn-icon").classList.add("fa-eraser");
       activeNote = null;
     }
-      $('#body-field').val('');
-      $('#body-field').css('background-color', 'white');
+    $('#input-field').val('');
+    $('#input-field').css('background-color', 'white');
   });
 
-  $('#listed').click(function(e){
-    var id = e.target.parentElement.parentElement.id;
-    var color = e.target.parentElement.style.backgroundColor;
+  $('#listed').click(function (e) {
+    var id = e.target.parentElement.id;
+    var color = e.target.style.backgroundColor;
     activeNote = id;
-    var bodySel = $('#' + id)[0].children[0].children[0].innerHTML;
-    $('#body-field').val(bodySel);
-    $('#body-field').css('background-color', color);
+    var bodySel = e.target.textContent;
+    $('#input-field').val(bodySel);
+    $('#input-field').css('background-color', color);
     document.getElementById("btn-icon").classList.remove("fa-eraser");
     document.getElementById("btn-icon").classList.add("fa-remove");
   });
 
-})
+});
